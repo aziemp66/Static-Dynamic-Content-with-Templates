afbf16bb6cbcc448d5abf2205bca67d78648bd93
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-07 12:02:24 +0700

    Outputting Repeated Content with EJS & Loops

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/restaurants", function (req, res) {
-    res.render("restaurants");
+    const filePath = path.join(__dirname, "data", "restaurants.json");
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    res.render("restaurants", {
+        numberOfRestaurants: storedRestaurants.length,
+        restaurants: storedRestaurants,
+    });
 });
 
 app.get("/confirm", function (req, res) {
